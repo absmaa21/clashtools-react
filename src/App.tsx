d752e01b0103313284f5a4ef3e5b3af0830f38d3
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2025-04-29 22:39:22 +0200

    Update App.tsx
    - remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import AdminDashboard from "./screens/admin/AdminDashboard.tsx";
 import {Route, Routes} from "react-router";
 import Index from "./screens";
 import UpgradeTracker from "./screens/UpgradeTracker.tsx";
-import LoginScreen from "./screens/LoginScreen.tsx";
-import RegisterScreen from "./screens/RegisterScreen.tsx";
 import Layout from "./components/Layout.tsx";
 
 function App() {
