04603431e57113a6d69fed507b2cef4a920b77c0
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2025-04-30 10:21:23 +0200

    add coc assets

--- src/components/upgrade-tracker/UpgradeCol.tsx
+++ src/components/upgrade-tracker/UpgradeCol.tsx
@@ -5,10 +5,13 @@ interface Props {
 }
 
 function UpgradeCol({entityLevel}: Props) {
+
+  const finishedDate = new Date(Date.now() + (entityLevel.timeUntilCompleted ?? 0)).toLocaleString()
+
   return (
     <TableCell sx={{ width: {sm: '33%', md: 200} }}>
       <Box sx={{display: 'flex', justifyContent: 'center', gap: 2}}>
-
+        {`${entityLevel.level} -- ${entityLevel.timeUntilCompleted ? finishedDate : ''}`}
       </Box>
     </TableCell>
   )
---
+++ src/utils/CocAssets.ts
@@ -0,0 +1,12 @@
+import {ResourceType} from "../enums/ResourceType.ts";
+
+export function getResourceByType(type: ResourceType): string {
+  switch (type) {
+    case ResourceType.GOLD: return 'https://static.wikia.nocookie.net/clashofclans/images/1/10/Gold.png'
+    case ResourceType.ELIXIR: return 'https://static.wikia.nocookie.net/clashofclans/images/4/43/Elixir.png'
+    case ResourceType.DARK_ELIXIR: return 'https://static.wikia.nocookie.net/clashofclans/images/4/4e/Dark_Elixir.png'
+    case ResourceType.SHINY_ORE: return 'https://static.wikia.nocookie.net/clashofclans/images/d/da/Shiny_Ore.png'
+    case ResourceType.GLOWY_ORE: return 'https://static.wikia.nocookie.net/clashofclans/images/7/72/Glowy_Ore.png'
+    case ResourceType.STARRY_ORE: return 'https://static.wikia.nocookie.net/clashofclans/images/0/07/Starry_Ore.png'
+  }
+}
