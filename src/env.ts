69cdd1c73bfc10cf8a3fd83c650383f9ffcfde0e
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2025-05-14 09:44:19 +0200

    change env

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
 export const ip_address: string = "localhost:8080"
 export const base_url: string = "http://" + ip_address
 
-export const skipNetwork: boolean = false
+export const skipNetwork: boolean = true
